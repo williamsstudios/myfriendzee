585b4204c24515b46854d44709ad06b5f8f93e9a
Matthew Williams <vickinmatt141123@gmail.com>
2025-11-03 00:08:50 -0500

    Minor Fix

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const db = require('./config/keys').mongoURI;
 mongoose
     .connect(
         db,
-        { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+        { useNewUrlParser: true, useUnifiedTopology: true }
     )
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
